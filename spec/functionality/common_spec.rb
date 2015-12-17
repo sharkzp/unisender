7438d0d4e06893277838d4ac1295d9030558a7af
droff <adroff@gmail.com>
2015-12-17 17:14:20 +0300

    fix tests

--- spec/functionality/common_spec.rb
+++ spec/functionality/common_spec.rb
@@ -4,10 +4,10 @@ describe UniSender::Client do
 
   it 'should get invalid key error' do
     answer = UniSender::Client.new("123abcd123").getLists
-    answer['error'].should_not be_nil
-    answer['code'].should == 'invalid_api_key'
+    expect(answer['error']).to be_truthy
+    expect(answer['code']).to eq 'invalid_api_key'
   end
 
-  specify{ test_client.get_lists.should == test_client.getLists}
+  specify { expect(test_client.get_lists).to eq test_client.getLists }
 
 end
